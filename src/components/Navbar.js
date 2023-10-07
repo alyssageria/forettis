e06488e0bf33d728e914c3939a328c8263e728a9
Alyssa Geria <alyssageria@gmail.com>
2023-10-06 18:11:09 -0700

    navbar general structure

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,53 +2,60 @@ import React from 'react';
 import forettisLogo from '../images/forettis-logo.png';
 import instagramIcon from '../images/instagram-icon.png';
 import yelpIcon from '../images/yelp-icon.png';
+import 'animate.css';
 
 function Navbar({ currentPage, handlePageChange }) {
     return (
         <div>
-            <div>
-                <img src={forettisLogo}></img>
-                <ul>
-                    <li>
+            <div className='flex justify-between p-3 animate__animated animate__fadeInDown'>
+                <img src={forettisLogo} className='h-12'></img>
+                <ul className='flex items-center'>
+                    <li className='p-2 text-xl hover:text-red hover:text-2xl delay-150 transition-all duration-200 ease-in-out'>
                         <button
                             onClick={() => handlePageChange('Home')}
                             className={currentPage === 'Home' ? 'active' : ''}
                         >Home</button>
                     </li>
-                    <li>
+                    <li className='p-2 text-xl hover:text-red hover:text-2xl delay-150 transition-all duration-200 ease-in-out'>
                         <button
                             onClick={() => handlePageChange('About')}
                             className={currentPage === 'About' ? 'active' : ''}
                         >About</button>
                     </li>
-                    <li>
+                    <li className='p-2 text-xl hover:text-red hover:text-2xl delay-150 transition-all duration-200 ease-in-out'>
                         <button
                             onClick={() => handlePageChange('Menu')}
                             className={currentPage === 'Menu' ? 'active' : ''}
                         >Menu</button>
                     </li>
-                    <li>
+                    <li className='p-2 text-xl hover:text-red hover:text-2xl delay-150 transition-all duration-200 ease-in-out'>
                         <button
                             onClick={() => handlePageChange('Reservations')}
                             className={currentPage === 'Reservations' ? 'active' : ''}
                         >Reservations</button>
                     </li>
-                    <li>
+                    <li className='p-2 text-xl hover:text-red hover:text-2xl delay-150 transition-all duration-200 ease-in-out'>
                         <button
                             onClick={() => handlePageChange('Catering')}
                             className={currentPage === 'Catering' ? 'active' : ''}
                         >Catering</button>
                     </li>
-                    <li>
+                    <li className='p-2 text-xl hover:text-red hover:text-2xl delay-150 transition-all duration-200 ease-in-out'>
                         <button
                             onClick={() => handlePageChange('Contact')}
                             className={currentPage === 'Contact' ? 'active' : ''}
                         >Contact</button>
                     </li>
+                    <li className='p-2 text-xl text-red hover:text-black hover:text-2xl delay-150 transition-all duration-200 ease-in-out'>
+                        <button
+                            onClick={() => handlePageChange('Order')}
+                            className={currentPage === 'Order' ? 'active' : ''}
+                        >Order Now</button>
+                    </li>
                 </ul>
-                <div>
-                    <img src={instagramIcon}></img>
-                    <img src={yelpIcon}></img>
+                <div className='flex'>
+                    <a href='https://www.instagram.com/forettis_/'><img src={instagramIcon} className='h-12 px-3'></img></a>
+                    <a href='https://www.yelp.com/biz/forettis-corona-del-mar-2'><img src={yelpIcon} className='h-12 px-3'></img></a>
                 </div>
             </div>
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'red': '#D21F25'
+      }
+    },
   },
   plugins: [],
 }
