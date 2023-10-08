7cb80b673da8a459654c34aceadf91cce49bba72
Alyssa Geria <alyssageria@gmail.com>
2023-10-08 09:58:24 -0700

    switched to using routes, progress on mobile nav menu

--- src/components/CurrentPage.js
+++ src/components/CurrentPage.js
@@ -1,30 +1,42 @@
 import React, { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from './Navbar';
 import Home from './pages/Home';
 import About from './pages/About';
 
 function CurrentPage() {
-    const [currentPage, setCurrentPage] = useState('Home');
+    // const [currentPage, setCurrentPage] = useState('Home');
 
-    const renderPage = () => {
-        if (currentPage === 'Home') {
-            return <Home />;
-        }
-        if (currentPage === 'About') {
-            return <About />;
-        }
-        // if (currentPage === 'Menu') {
-        //     return <Menu />;
-        // }
-    };
+    // const renderPage = () => {
+    //     if (currentPage === 'Home') {
+    //         return <Home />;
+    //     }
+    //     if (currentPage === 'About') {
+    //         return <About />;
+    //     }
+    //     // if (currentPage === 'Menu') {
+    //     //     return <Menu />;
+    //     // }
+    // };
 
-    const handlePageChange = (page) => setCurrentPage(page);
+    // const handlePageChange = (page) => setCurrentPage(page);
 
+    // return (
+    //     <div >
+    //         <Navbar currentPage={currentPage} handlePageChange={handlePageChange} />
+    //         {renderPage()}
+    //     </div>
+    // );
     return (
-        <div >
-            <Navbar currentPage={currentPage} handlePageChange={handlePageChange} />
-            {renderPage()}
-        </div>
+        <Router>
+            <div>
+                <Navbar />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/about" element={<About />} />
+                </Routes>
+            </div>
+        </Router>
     );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import forettisLogo from '../images/forettis-logo.png';
 import instagramIcon from '../images/instagram-icon.png';
 import yelpIcon from '../images/yelp-icon.png';
@@ -13,33 +14,43 @@ function Navbar({ currentPage, handlePageChange }) {
 
     return (
         <div>
-            <div className='flex justify-between p-3 animate__animated animate__fadeInDown'>
-                <img src={forettisLogo} className='h-12'></img>
-                {isMenuOpen ? (
+            {isMenuOpen ? (
+                <div>
                     <div>
-                        <ul>
-                            <li>Home</li>
-                            <li>About</li>
-                            <li>Menu</li>
-                            <li>Reservations</li>
-                            <li>Catering</li>
-                            <li>Contact</li>
-                            <li>Order Now</li>
+                        <div className='xs:flex justify-between lg:hidden p-3'>
+                            <img src={forettisLogo} className='h-12'></img>
+                            <button className='text-4xl text-red p-2' onClick={toggleMenu}>☰</button>
+                        </div>
+                    </div>
+                    <div className='h-[90vh] animate__animated animate__fadeInDown flex items-center justify-center'>
+                        <ul className='text-center text-2xl'>
+                            <li className='p-2'>Home</li>
+                            <li className='p-2'>About</li>
+                            <li className='p-2'>Menu</li>
+                            <li className='p-2'>Reservations</li>
+                            <li className='p-2'>Catering</li>
+                            <li className='p-2'>Contact</li>
+                            <li className='p-2'>Order Now</li>
                         </ul>
                     </div>
-                ) : (
+                </div>
+            ) : (
+                <div className='flex justify-between p-3 animate__animated animate__fadeInDown'>
+                    <img src={forettisLogo} className='h-12'></img>
                     <ul className='flex items-center xs:hidden lg:flex'>
                         <li className='p-2 text-xl hover:text-red hover:text-2xl delay-150 transition-all duration-200 ease-in-out'>
-                            <button
-                                onClick={() => handlePageChange('Home')}
+                            <Link
+                                to='/'
+                                // onClick={() => handlePageChange('Home')}
                                 className={currentPage === 'Home' ? 'active' : ''}
-                            >Home</button>
+                            >Home</Link>
                         </li>
                         <li className='p-2 text-xl hover:text-red hover:text-2xl delay-150 transition-all duration-200 ease-in-out'>
-                            <button
-                                onClick={() => handlePageChange('About')}
+                            <Link
+                                to='/about'
+                                // onClick={() => handlePageChange('About')}
                                 className={currentPage === 'About' ? 'active' : ''}
-                            >About</button>
+                            >About</Link>
                         </li>
                         <li className='p-2 text-xl hover:text-red hover:text-2xl delay-150 transition-all duration-200 ease-in-out'>
                             <button
@@ -72,18 +83,17 @@ function Navbar({ currentPage, handlePageChange }) {
                             >Order Now</button>
                         </li>
                     </ul>
-                )}
-
-                <div className='flex items-center xs:hidden lg:flex'>
-                    <a href='https://www.instagram.com/forettis_/'><img src={instagramIcon} className='h-9 px-3'></img></a>
-                    <a href='https://www.yelp.com/biz/forettis-corona-del-mar-2'><img src={yelpIcon} className='h-9 px-3'></img></a>
+                    <div className='flex items-center xs:hidden lg:flex'>
+                        <a href='https://www.instagram.com/forettis_/'><img src={instagramIcon} className='h-9 px-3'></img></a>
+                        <a href='https://www.yelp.com/biz/forettis-corona-del-mar-2'><img src={yelpIcon} className='h-9 px-3'></img></a>
+                    </div>
+                    <div className='xs:flex justify-end lg:hidden'>
+                        <button className='text-4xl text-red p-2' onClick={toggleMenu}>☰</button>
+                    </div>
                 </div>
-
-                <div className='xs:flex flex-col lg:hidden'>
-                    <button className='text-4xl text-red p-2' onClick={toggleMenu}>☰</button>
-                </div>
-            </div>
+            )}
         </div>
+
     )
 }
 
